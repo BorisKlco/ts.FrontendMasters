d4c3ff09693b9a391a4eb7b4d3bd797d5e9256c9
BorisKlco <klco@pm.me>
2023-08-15 13:37:37 +0200

    Explicitly define return types

--- TypeScript Fundamentals v3/func.ts
+++ TypeScript Fundamentals v3/func.ts
@@ -24,6 +24,10 @@ const flipCoin: FlipType = (choice, stake) => {
 
 console.log(flipCoin("tail", 50));
 
+////////////////////
+//
+//
+//
 // void != undefined
 // Function overload
 
@@ -42,3 +46,38 @@ const form = document.getElementsByTagName("form")[0];
 
 handleEvent(frame, (val) => {});
 handleEvent(form, (val) => {});
+
+//////////////////////////////////
+//
+//
+//
+// Explicitly define return types
+
+export async function getData(url: string) {
+  const resp = await fetch(url);
+  const data = (await resp.json()) as { prop: string[] };
+  return data;
+}
+
+function fetchData() {
+  getData("url").then((result) => {
+    console.log(result.prop.join(","));
+    //result.prop IS define
+  });
+}
+
+//// Adding statement if resp is ok
+export async function getDataIf(url: string) {
+  const resp = await fetch(url);
+  if (resp.ok) {
+    const data = (await resp.json()) as { prop: string[] };
+    return data;
+  }
+}
+
+function fetchDataIf() {
+  getDataIf("url").then((result) => {
+    if (result) return console.log(result.prop.join(","));
+    //result.prop CANT be define, what if fetch fail
+  });
+}
